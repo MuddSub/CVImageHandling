4c7eba0f03ba3325b2dd9b5708c981f12951a269
Hannah Lu <hannahlu1207@yahoo.com>
2020-12-28 20:48:25 -0800

    minor cosmetic edits

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 <h2>
-    MuddSub Image Labeling skdfskf
+    MuddSub Image Labeling
 </h2>
 <p>
     This website is used to label and put bounding boxes on images that will be used to train our neural network. 
@@ -10,7 +10,7 @@
 <p class="test">
     In order to sign up to label images at the competition, please make an account on the login page. We need to make sure our bounding boxes are exact, so please take a look at the example page before doing any labeling. Once you've done that and we have images, they will be distributed to you on the label page. 
 </p>
-<ul>
+<ul id="nav">
 here is a list of links to the other pages:
     <li><a href="example.html">example</a></li>
     <li><a href="login.html">login</a></li>
--- label.php
+++ label.php
@@ -12,6 +12,14 @@
 <p>
     Needs to export image name, labels, and bounding box to a text file
 </p>
+<ul id="nav">
+here is a list of links to the other pages:
+    <li><a href="example.html">example</a></li>
+    <li><a href="login.html">login</a></li>
+    <li><a href="label.php">labeling</a></li>
+    <li><a href="verify.html">verify</a></li>
+    <li><a href="upload.html">image upload</a></li>
+</ul>
 
 
 <?php include_once("includes/footer.php")?>
